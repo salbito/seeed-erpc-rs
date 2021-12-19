a2367759b8dacbed869b18a46444d69d4da4b075
Sean Albito <me@salbito.com>
2021-12-19 15:49:09 -0800

    Add/Update/Include IDs for BLE

--- src/ids.rs
+++ src/ids.rs
@@ -29,6 +29,8 @@ pub enum Service {
     BLEHost = 2,
     BLEGap = 3,
     BLEGapBone = 4,
+    BLEGattClient = 11,
+    BLEGattServer = 12,
     BLECallback = 13,
     Wifi = 14,
     TCPIP = 15,
@@ -43,6 +45,8 @@ impl From<u8> for Service {
             2 => Service::BLEHost,
             3 => Service::BLEGap,
             4 => Service::BLEGapBone,
+            11 => Service::BLEGattClient,
+            12 => Service::BLEGattServer,
             13 => Service::BLECallback,
             14 => Service::Wifi,
             15 => Service::TCPIP,
@@ -62,6 +66,113 @@ pub enum SystemRequest {
 
 impl From<SystemRequest> for u8 {
     fn from(r: SystemRequest) -> u8 {
+        r as u8
+    }
+}
+
+/// Wio Terminal request IDs for the BLEHost service
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(unused)]
+pub enum BLEHostRequest {
+    InitID = 1,
+    StartID = 2,
+    DeInitID = 3,
+}
+
+impl From<BLEHostRequest> for u8 {
+    fn from(r: BLEHostRequest) -> u8 {
+        r as u8
+    }
+}
+
+/// Wio Terminal request IDs for the BLEGap service
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(unused)]
+pub enum BLEGapRequest {
+    SetParamID = 1,
+    GetParamID = 2,
+    SetPairableModeID = 3,
+}
+
+impl From<BLEGapRequest> for u8 {
+    fn from(r: BLEGapRequest) -> u8 {
+        r as u8
+    }
+}
+
+/// Wio Terminal request IDs for the BLEGapBone service
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(unused)]
+pub enum BLEGapBoneRequest {
+    LeBondSetParamID = 1,
+    LeBondGetParamID = 2,
+    LeBondPairID = 3,
+    LeBondGetDisplayKeyID = 4,
+    LeBondPasskeyInputConfirmID = 5,
+    LeBondOobInputConfirmID = 6,
+    LeBondJustWorkConfirmID = 7,
+    LeBondPasskeyDisplayConfirmID = 8,
+    LeBondUserConfirmID = 9,
+    LeBondCfgLocalKeyDistributeID = 10,
+    LeBondClearAllKeysID = 11,
+    LeBondDeleteByIdxID = 12,
+    LeBondDeleteByBdID = 13,
+    LeBondGetSecLevelID = 14,
+}
+
+impl From<BLEGapBoneRequest> for u8 {
+    fn from(r: BLEGapBoneRequest) -> u8 {
+        r as u8
+    }
+}
+
+/// Wio Terminal request IDs for the BLEGattClient service
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(unused)]
+pub enum BLEGattClientRequest {
+    BLEClientInitID = 1,
+    BLEClientAddClientID = 2,
+    ClientInitID = 3,
+    ClientAllPrimarySrvDiscoveryID = 4,
+    ClientByUuidSrvDiscoveryID = 5,
+    ClientByUuid128SrvDiscoveryID = 6,
+    ClientRelationshipDiscoveryID = 7,
+    ClientAllCharDiscoveryID = 8,
+    ClientByUuidCharDiscoveryID = 9,
+    ClientByUuid128CharDiscoveryID = 10,
+    ClientAllCharDescriptorDiscoveryID = 11,
+    ClientAttrReadID = 12,
+    ClientAttrReadUsingUuidID = 13,
+    ClientAttrWriteID = 14,
+    ClientAttrIndConfirmID = 15,
+}
+
+impl From<BLEGattClientRequest> for u8 {
+    fn from(r: BLEGattClientRequest) -> u8 {
+        r as u8
+    }
+}
+
+/// Wio Terminal request IDs for the BLEGattServer service
+#[derive(Debug, Copy, Clone, PartialEq)]
+#[allow(unused)]
+pub enum BLEGattServerRequest {
+    BLEServerInitID = 1,
+    BLECreateServiceID = 2,
+    BLEDeleteServiceID = 3,
+    BLEServiceStartID = 4,
+    BLEGetServiceHandleID = 5,
+    BLECreateCharID = 6,
+    BLECreateDescID = 7,
+    ServerSendDataID = 8,
+    BLEServerGetAttrValueID = 9,
+    ServerExecWriteConfirmID = 10,
+    ServerAttrWriteConfirmID = 11,
+    ServerAttrReadConfirmID = 12,
+}
+
+impl From<BLEGattServerRequest> for u8 {
+    fn from(r: BLEGattServerRequest) -> u8 {
         r as u8
     }
 }
